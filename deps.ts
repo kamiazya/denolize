9ee80afb63c6d622d6d091d4d3d9bb1e271ceb68
kamiazya <yuki@kamiazya.tech>
2021-05-08 21:52:00 +0900

    fix dependencies

--- deps.ts
+++ deps.ts
@@ -1,6 +1,5 @@
 import * as fs from "https://deno.land/std@0.95.0/fs/mod.ts";
 import * as path from "https://deno.land/std@0.95.0/path/mod.ts";
-// @deno-types="https://deno.land/x/typescript@v3.9.7/lib/typescript.d.ts"
-import ts from "https://dev.jspm.io/typescript@3.9.7/lib/typescript.js";
-import { snakeCase } from "https://deno.land/x/case@v2.0.0/mod.ts";
+import ts from "https://esm.sh/typescript@3.9.7";
+import { snakeCase } from "https://deno.land/x/case@v2.1.0/mod.ts";
 export { ts, fs, path, snakeCase };
